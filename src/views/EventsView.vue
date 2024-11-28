834b732978475fceb2cfd6c0519660eef71d9b4a
JakubBansky <jakub.bansky@gmail.com>
2024-11-28 17:58:42 +0100

    gh pages update

--- src/views/EventsView.vue
+++ src/views/EventsView.vue
@@ -15,14 +15,14 @@
       events, workshops, and opportunities to grow with us.
       For any questions or further information, feel free to reach out to us at 
       [your contact email]. Let's make this season impactful together!"
-			mainPhoto="src/assets/sRM_2024.jpg"
-			:photos="['src/assets/sRM_2024.jpg', 'src/assets/hoodie.jpg']" />
+			mainPhoto="../assets/sRM_2024.jpg"
+			:photos="['../assets/sRM_2024.jpg', '../assets/hoodie.jpg']" />
 		<Event
 			class="p-4"
 			title="sRM"
 			date="2024-10-27"
 			description="BEST Bratislava recently hosted an inspiring Spring Regional Meeting, bringing together members from various chapters to collaborate, connect, and enhance our mission. This dynamic event was filled with workshops, discussions, and team-building activities designed to strengthen our local networks and empower our members to excel in their roles."
 			mainPhoto="src/assets/sRM_2024.jpg"
-			:photos="['src/assets/lbgs.png', 'src/assets/hoodie.jpg', 'src/assets/joinBB.png']" />
+			:photos="['../assets/lbgs.png', '../assets/hoodie.jpg', '../assets/joinBB.png']" />
 	</div>
 </template>
